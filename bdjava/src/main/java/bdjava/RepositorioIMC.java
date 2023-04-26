260621f8f68b7521e544ed1b4f67c15096a4fd57
valexx55 <valexx55@gmail.com>
2023-04-26 21:28:57 +0200

    dao y docu

---
+++ bdjava/src/main/java/bdjava/RepositorioIMC.java
@@ -0,0 +1,15 @@
+package bdjava;
+
+public class RepositorioIMC {
+	
+	//clase de acceso a datos
+	// DAO Data Acces Object
+	//Persistencia
+	
+	//TODO VAMOS A HACER LAS OPERACIONES CON BASE DATOS DE:
+	//1 INSERTAR PERSONA
+	//2 INSETAR REGISTRO HISTORICO DE IMC
+	//3 CONSULTAR REGISTROS IMC TODOS
+	//4 CONSULTAR REGISTROS POR RANGO
+
+}
